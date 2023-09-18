ddb46e8fce2d0e09af48df058da5e4a39cf83d52
braindesigner <metabraindesigner@gmail.com>
2023-09-18 12:34:44 +0900

    feat: Print 'fizz' if i is multiple of 3

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 def print_hello(num: int):
     """
     Print hello
@@ -5,3 +6,8 @@ def print_hello(num: int):
     for i in range(num):
         print(f'{i} 안녕')
 
+if __name__=='__main__':
+    print_hello(10)
+
+for num in range(10)
+    print(f'hello for {num+1}th times')
